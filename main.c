65678cee26acbdb3e4f91f56842e1e752e39e811
Milan Vu <milanvu@Milans-MacBook-Pro.local>
2017-12-21 12:33:06 +0100

    Working for first two tests

--- main.c
+++ main.c
@@ -2,8 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-
-
 int isChar ( char c ){
   if ( (c > 64 && c < 91) || (c > 96 && c < 123) )
     return 1;
@@ -11,22 +9,21 @@ int isChar ( char c ){
 }
 
 int getWordLength ( char * array, int adr ){
-  while ( isChar(array[adr]) )
+  int length = 0;
+  while ( isChar(array[adr]) ){
+    ++length;
     ++adr;
-  return adr - 1;
-}
-
-int getLongerWord ( int a, int b ){
-  if ( a > b )
-    return a;
-  else
-    return b;
+  }
+  return length;
 }
 
 int main ( void ){
-  int count = 1, count2 = 0, line = 1;
+  int count = 1, count2 = 0;
+  int inEnd = 0, inLine = 1;
+  int found = 0;
   char * text = (char *) malloc((1000)*sizeof(char));
   char * searched_string = (char *) malloc((1000)*sizeof(char));
+  int * lineMem = (int *) malloc((1000)*sizeof(int));
   text[0] = ' ';
 
   printf("Text:\n");
@@ -34,30 +31,50 @@ int main ( void ){
   while ( 1 ){
 
     text[count] = getchar();
+    lineMem[count] = inLine;
+
+    if ( text[count] > 64 && text[count] < 91 ){
+      text[count] += 32;
+    }
+
+
+
 
     if ( text[count] == EOF ){
       printf("Nespravny vstup.\n");
       free(text);
       free(searched_string);
+      free(lineMem);
       return 0;
     }
 
 
 
-    if ( text[count] == '\n' && text[count-1] == '\n' ) {
-      printf("%d\n", getWordLength(text, 1));
+    if ( text[count] == '\n' ){
+      ++inLine;
+      ++inEnd;
+    }
+    else{
+      if ( inEnd > 0 )
+        --inEnd;
+    }
+
+    if ( inEnd == 2 ){
       break;
     }
 
 
     //* deletes extra non-characters (2 or more), leaves new_lines (for line count)
     if ( isChar(text[count]) == 0
-    && isChar(text[count-1]) == 0
-    && text[count] != '\n'
-    && text[count-1] != '\n' ){
+    && isChar(text[count-1]) == 0 ){
+      if ( text[count] == '\n' || text[count-1] == '\n' )
+        text[count-1] = '\n';
       --count;
     }
     //*
+
+    if ( isChar(text[count]) == 0 && text[count] != '\n' )
+      text[count] = ' ';
 
     ++count;
 
@@ -67,26 +84,50 @@ int main ( void ){
   }
 
   printf("Hledani:\n");
+
   int err = 0;
   while ( 1 ){
+
     searched_string[count2] = getchar();
+
+    if ( searched_string[0] == '\n' ){
+      printf("Neplatny dotaz\n");
+    }
+
+    if ( searched_string[count2] > 64 && searched_string[count2] < 91 ){
+      searched_string[count2] += 32;
+    }
+
+
 
     if ( searched_string[count2] == EOF ){
       free(text);
       free(searched_string);
+      free(lineMem);
       return 0;
     }
 
+    //* deletes extra non-characters (2 or more), leaves new_lines (for line count)
+    if ( isChar(searched_string[count2]) == 0
+    && isChar(searched_string[count2-1]) == 0 ){
+      --count2;
+    }
+
     ++count2;
-    if ( count2 % 1000 == 0 )
+
+    if ( count2 % 1000 == 0 ){
       searched_string = (char *) realloc(searched_string, (count2+1000)*sizeof(char));
+      lineMem = (int *) realloc(lineMem, (count2+1000)*sizeof(int));
+    }
+
+    if ( isChar(searched_string[count2]) == 0 && searched_string[count2] != '\n' )
+      searched_string[count2] = ' ';
+
 
     if ( searched_string[count2 - 1] == '\n' ){
 
-      printf("Nalezeno: ");
       for ( int i = 0; i < count - 2; ++i ){
-        if ( text[i] == '\n' )
-          ++line;
+
         if ( isChar(text[i]) == 0 && isChar(text[i+1]) == 1 && text[i+1] == searched_string[0] ){
 
           for ( int j = 0; j < count2 - 1; ++j ){
@@ -95,23 +136,30 @@ int main ( void ){
               break;
             }
           }
-          if ( err == 0 ){
-            printf("%d, ", line);
+          if ( err == 0 && (count2 - 1) >= getWordLength(text, i+1)){
+            ++found;
+            if ( found == 1 ){
+              printf("Nalezeno: ");
+            }
+            if ( found > 1 ){
+              printf(", ");
+            }
+
+            printf("%d", lineMem[i+1]);
+
           }
           err = 0;
         }
       }
+      if ( found == 0 )
+        printf("Nenalezeno");
       printf("\n");
-      line = 1;
       count2 = 0;
+      found = 0;
     }
-
-
-
   }
 
-  printf("%s\n", text);
-
+  free(lineMem);
   free(text);
   free(searched_string);
   return 0;
--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #gcc -fsanitize=address -g main.c
-g++ -std=c++11 -Wall -pedantic -Wno-long-long -O3 main.c
+gcc -Wall -pedantic -Wno-long-long -O3 main.c
 #gcc -O3 main.c
 
-for i in 0 1
+for i in 0 1 2
 do
   echo "test "$i":"
   ./a.out < tests/000${i}_in.txt > tests/000${i}.out
